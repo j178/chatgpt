29fbd78f3063b6e76ccf11d3f88b09ddb41a3092
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-12-21 19:36:16 +0900

    json

--- main.go
+++ main.go
@@ -3,20 +3,73 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
 	"github.com/PullRequestInc/go-gpt3"
 )
 
+type payload struct {
+	Text string `json:"text"`
+}
+
+type response struct {
+	EOF  bool   `json:"eof"`
+	Text string `json:"text"`
+}
+
+func doJson(client gpt3.Client, r io.Reader, w io.Writer) error {
+	enc := json.NewEncoder(w)
+	dec := json.NewDecoder(r)
+	for {
+		var p payload
+		err := dec.Decode(&p)
+		if err != nil {
+			return err
+		}
+		err = client.CompletionStreamWithEngine(
+			context.Background(),
+			gpt3.TextDavinci003Engine,
+			gpt3.CompletionRequest{
+				Prompt: []string{
+					p.Text,
+				},
+				MaxTokens:   gpt3.IntPtr(3000),
+				Temperature: gpt3.Float32Ptr(0),
+			}, func(resp *gpt3.CompletionResponse) {
+				enc.Encode(response{EOF: false, Text: resp.Choices[0].Text})
+			},
+		)
+		if err != nil {
+			return err
+		}
+		err = enc.Encode(response{EOF: true})
+		if err != nil {
+			return err
+		}
+	}
+}
+
 func main() {
+	var j bool
+	flag.BoolVar(&j, "json", false, "json input/output")
+	flag.Parse()
+
 	apiKey := os.Getenv("CHATGPT_API_KEY")
 	if apiKey == "" {
 		log.Fatal("Missing CHATGPT_API KEY")
 	}
 
 	client := gpt3.NewClient(apiKey)
+
+	if j {
+		log.Fatal(doJson(client, os.Stdin, os.Stdout))
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("> ")
